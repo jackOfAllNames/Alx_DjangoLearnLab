7b1ceb4a02789619c299330a8667ce3fbb7b859f
Olaitan Adedokun <kenzdex@gmail.com>
2025-07-15 15:03:44 +0100

    Implement Class-based View

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import display_books
+from .views import display_books, libraries_list, library_details
 
 urlpatterns = [
-    path('', display_books),
+    path('books/', display_books),
+    path('libraries/', libraries_list),
+    path('library/<int:pk>', library_details)
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Book
+from django.views.generic import DetailView
+from .models import Book, Library
 
 # Create your views here.
 def display_books(request):
@@ -9,3 +10,16 @@ def display_books(request):
         'books': all_books
     }
     return render(request, "relationship_app/list_books.html", context)
+
+def libraries_list(request):
+    libraries = Library.objects.prefetch_related('books')
+    return render(request, 'libraries.html', {'libraries': libraries})
+
+def library_details(request, pk):
+    library = Library.objects.get(pk=pk)
+    return render(request, 'library_detail.html', {'library': library})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "library_detail.html"
+    queryset = Library.objects.prefetch_related("books__author")

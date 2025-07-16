28f1d1ebcfbca53a2dcd188a413d225eabf7a10e
Olaitan Adedokun <kenzdex@gmail.com>
2025-07-16 20:23:32 +0100

    add the urls and views files

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from .views import display_books, library_details
-from .views import list_books
+from .views import list_books, CustomLoginView, CustomLogoutView, register_view
 from .views import LibraryDetailView
 
 urlpatterns = [
@@ -9,4 +9,7 @@ urlpatterns = [
     path('libraries/', list_books),
     # path('library/<int:pk>', library_details)
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', register_view, name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
+from django.contrib.auth.views import LoginView, LogoutView
 from .models import Book
 from .models import Library
+from .forms import RegisterForm
 
 # Create your views here.
 def display_books(request):
@@ -24,3 +27,20 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     queryset = Library.objects.prefetch_related("books__author")
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'

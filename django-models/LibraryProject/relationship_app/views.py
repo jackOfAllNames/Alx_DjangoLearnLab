725f2c84e2db1681335dc56860ca46250ecbdd7c
Olaitan Adedokun <kenzdex@gmail.com>
2025-07-17 09:58:41 +0100

    setting permissions and authentications

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import login
-from django.shortcuts import render, redirect, HttpResponse
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.views import LoginView, LogoutView
 from .models import Book
 from .models import Library
@@ -48,3 +49,20 @@ class CustomLogoutView(LogoutView):
 
 def home(request):
     return HttpResponse(f"Hello, {request.user.username}!" if request.user.is_authenticated else "Welcome Guest")
+
+def role_required(role):
+    def decorator(user):
+        return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == role
+    return user_passes_test(decorator)
+
+@role_required('Admin')
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+@role_required('Librarian')
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+@role_required('Member')
+def member_view(request):
+    return render(request, 'member_view.html')

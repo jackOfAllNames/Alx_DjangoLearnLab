98a9bbae0a28652a360e1b59e6da0d10e7c08f56
Olaitan Adedokun <kenzdex@gmail.com>
2025-07-30 09:49:12 +0100

    implement the book list api  using the DRF

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index, BookList
 
 urlpatterns = [
     path('', index),
+    path('views/', BookList.as_view(), name='book-list'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render, HttpResponse
+from .serializers import BookSerializer
+from rest_framework import generics
+from .models import Book
 
 # Create your views here.
 def index(request):
     return HttpResponse('Hello, World!')
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer

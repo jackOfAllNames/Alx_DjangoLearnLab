988f7bf653faf7dee5e9d22ec85c714ffcc09571
Olaitan Adedokun <kenzdex@gmail.com>
2025-08-05 06:50:29 +0100

    implement generic views and url patterns

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from django.shortcuts import render, HttpResponse
-from .serializers import AuthorSerializer
+from .serializers import AuthorSerializer, BookSerializer
 from .models import Author, Book
 
 def index(request):
@@ -9,3 +9,25 @@ def index(request):
 class ListAuthors(generics.ListAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+
+class ListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class DetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class CreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class UpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'pk'
+
+class DeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'pk'

7e53093fdeb90944f0753be3cf3fa3a0d4daf92e
Olaitan Adedokun <kenzdex@gmail.com>
2025-08-11 16:42:51 +0100

    created the homeview

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, TemplateView
+from .forms import CustomUserRegistrationForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+class HomeView(TemplateView):
+    template_name = 'blog/index.html'
+
+
+class SignUpView(CreateView):
+    form_class = CustomUserRegistrationForm
+    success_url = reverse_lazy('login')

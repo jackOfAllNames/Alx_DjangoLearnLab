76d293bf6df625012253da793aece65e8d024c75
Olaitan Adedokun <kenzdex@gmail.com>
2025-08-13 10:56:15 +0100

    implemented the login page and change the home page to include login and logout

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import HomeView, SignUpView, LoginView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('register/', SignUpView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login')
+    path('accounts/', include('django.contrib.auth.urls')),
+    # path('register/', SignUpView.as_view(), name='register'),
+    # path('login/', LoginView.as_view(), name='login')
 ]
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd parties
+    'crispy_forms',
+    'crispy_tailwind',
+
     # Local apps
     'blog',
 ]
@@ -57,7 +62,7 @@ ROOT_URLCONF = 'django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates', os.path.join(BASE_DIR, 'blog', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,3 +137,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'tailwind'
+CRISPY_TEMPLATE_PACK = 'tailwind'
+
+LOGIN_REDIRECT_URL = 'home'

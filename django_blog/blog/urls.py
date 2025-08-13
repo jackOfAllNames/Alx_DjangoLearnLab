6547a5881e3e47ba478b02e19779f77709b8e00e
Olaitan Adedokun <kenzdex@gmail.com>
2025-08-14 00:09:00 +0100

    Use the base url template

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('login/', LogInView.as_view(), name='login'),
     path('logout/', LogOutView.as_view(), name='logout'),
     path('register/', SignUpView.as_view(), name='register'),
+    path('posts/', LogOutView.as_view(), name='posts'),
+    path('profile/', LogOutView.as_view(), name='profile')
 
     # path('accounts/', include('django.contrib.auth.urls')),
     # path('accounts/register/', SignUpView.as_view(), name='signup'),

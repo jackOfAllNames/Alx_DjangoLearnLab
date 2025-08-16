a7a8109e40116f108d34ceb0698123494672171c
Olaitan Adedokun <kenzdex@gmail.com>
2025-08-16 19:49:49 +0100

    create the tag model and linking to the post

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
 
 
 class Comment(models.Model):
@@ -14,3 +15,10 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=72, unique=True)
+
+    def __str__(self):
+        return self.name

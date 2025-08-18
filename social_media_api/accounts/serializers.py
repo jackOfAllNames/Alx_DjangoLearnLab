6d68d0d84f2ebbf5520ce4fe4b9486819a297b0c
Olaitan Adedokun <kenzdex@gmail.com>
2025-08-18 22:45:05 +0100

    user serializer to store the password

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,6 +5,7 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers', 'password']
@@ -17,6 +18,5 @@ class UserSerializer(serializers.ModelSerializer):
             email=validated_data['email'],
             bio=validated_data.get('bio', ''),
         )
-        user.set_password(validated_data['password'])
         user.save()
         return user

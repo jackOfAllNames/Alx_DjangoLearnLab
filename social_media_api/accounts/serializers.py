5f7921755455273c993877371edd1e0c14bd6b80
Olaitan Adedokun <kenzdex@gmail.com>
2025-08-19 00:02:17 +0100

    used the method name instead of a variable for the User

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
@@ -13,7 +14,7 @@ class UserSerializer(serializers.ModelSerializer):
         read_only_fields = ('id', 'followers')
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             bio=validated_data.get('bio', ''),

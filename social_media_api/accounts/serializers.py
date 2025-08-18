bf41a28c38b12ce5225e089a1f3931c0fba6984a
Olaitan Adedokun <kenzdex@gmail.com>
2025-08-19 00:03:30 +0100

    made a correction

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -6,6 +6,7 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    # Token.objects.create
     password = serializers.CharField()
     class Meta:
         model = User

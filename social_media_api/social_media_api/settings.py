c3e007700fb9102fd08b5e8c8009732a7db2a386
Olaitan Adedokun <kenzdex@gmail.com>
2025-08-24 19:58:20 +0100

    debug to false on prod

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gv@s!5m70fkk$#ozsbjjzm4_(92j9f)(!e(-__at$cyxy+9qb+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 

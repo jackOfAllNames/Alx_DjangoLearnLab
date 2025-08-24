ea9694dd34273c5022f6f38898b03611bd4c28ed
Olaitan Adedokun <kenzdex@gmail.com>
2025-08-24 20:01:31 +0100

    added path for db

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -88,6 +88,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'blog',
+    #     'USER': 'bloguser',
+    #     'PASSWORD': 'Vnp-1234',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    # }
 }
 
 
